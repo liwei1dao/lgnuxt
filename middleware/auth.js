99c92bae54d89bb03b322f9c5392008feee01cb9
liwei1dao <liwei1dao@gmail.com>
2021-06-19 14:20:49 +0800

    上传代码

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,18 @@
-export default function ({ app, redirect, store }) {
+export default function ({ app, redirect, cookies, store }) {
   if (!store.state.user) {
-    app.$getuserinfo(null).then(response => {
-      const { data } = response
-      app.$cookies.set('User-Token', data.Token)
-      store.commit('Set_user', data)
-      return
-    }).catch(error => {
-      console.log("error:" + error)
-      app.$message.error(error.message)
-      return redirect('/login');
-    })
+    const token = cookies.get('User-Token')
+    if (token != null) {
+      app.$getuserinfo(null).then(response => {
+        const { data } = response
+        app.$cookies.set('User-Token', data.Token)
+        store.commit('Set_user', data)
+        return
+      }).catch(error => {
+        console.log("error:" + error)
+        app.$message.error(error.message)
+        return redirect('/login');
+      })
+    }
   }
+  return
 }
